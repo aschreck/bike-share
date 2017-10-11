9cc407567d21d9f2e0815cb1ef58835623669526
Adrian Lara <adrianblara@gmail.com>
2017-10-10 19:28:54 -0600

    fix user sees homepage feature test

--- spec/features/user_sees_homepage_or_dashboard_spec.rb
+++ spec/features/user_sees_homepage_or_dashboard_spec.rb
@@ -2,14 +2,9 @@
     it "they see the contents of the homepage/dashboard" do
       visit '/'
 
-      within("h1") do
-        expect(page).to have_content("Welcome to BikeShare!")
-      end
-      expect(page).to have_link("All Stations")
-      expect(page).to have_link("Add Station")
-      expect(page).to have_link("All Trips")
-      expect(page).to have_link("Add Trip")
-      expect(page).to have_link("All Conditions")
-      expect(page).to have_link("Add Condition")
+      expect(page).to have_content(/bikeshare/i)
+      expect(page).to have_link("All")
+      expect(page).to have_link("New")
+      expect(page).to have_link("Dashboard")
     end
   end
